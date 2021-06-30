f837843f5976002546e75788bbe2061efaf832f0
Fenomen-Alex <alexfenomen@gmail.com>
2021-06-30 17:59:28 +0300

    editing styles

--- src/components/SignupForm/Animator.tsx
+++ src/components/SignupForm/Animator.tsx
@@ -4,7 +4,6 @@ import { motion } from 'framer-motion';
 const Animator = ({children}: JSX.ElementChildrenAttribute) => {
   return (
       <motion.div
-          style={{ position: 'absolute' }}
           initial={{ x: 200, scale: 0.8, opacity: 0 }}
           animate={{ x: 0, scale: 1, opacity: 1 }}
           exit={{ x: -200, scale: 0.8, opacity: 0 }}
